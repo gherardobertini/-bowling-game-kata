e078fb4ae10bd4cd085d03c4252d50d22ad6d280
Gherardo Bertini <gherardo.bertini@gmail.com>
2022-10-18 16:52:37 +0200

    Feature/fourth test (#4)
    * add failing testOneStrikes
    
    * update Game with strike
    
    * refactor
    
    * add isStrike method
    
    * add rollStrike method in test
    
    Co-authored-by: Gherardo Bertini <gherardo.bertini@kiwitron.it>

--- src/Game.php
+++ src/Game.php
@@ -17,17 +17,41 @@ class Game
         $score = 0;
         $frameIndex = 0;
         for ($frame = 0; $frame < 10; $frame++) {
-            if ($this->isSpare($frameIndex)) {
-                $score += 10 + $this->rolls[$frameIndex + 2];
+            if ($this->isStrike($frameIndex)) {
+                $score += 10 + $this->strikeBonus($frameIndex);
+                $frameIndex++;
+            } else if ($this->isSpare($frameIndex)) {
+                $score += 10 + $this->spareBonus($frameIndex);
+                $frameIndex += 2;
             } else {
-                $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+                $score += $this->sumOfBallsInFrame($frameIndex);
+                $frameIndex += 2;
             }
-            $frameIndex += 2;
         }
         return $score;
     }
 
-    protected function isSpare(int $frameIndex)
+    private function isStrike(int $frameIndex): bool
+    {
+        return $this->rolls[$frameIndex] == 10;
+    }
+
+    private function sumOfBallsInFrame(int $frameIndex): int
+    {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+    }
+
+    private function spareBonus(int $frameIndex): int
+    {
+        return $this->rolls[$frameIndex + 2];
+    }
+
+    private function strikeBonus(int $frameIndex): int
+    {
+        return $this->rolls[$frameIndex + 1] + $this->rolls[$frameIndex + 2];
+    }
+
+    private function isSpare(int $frameIndex): bool
     {
         return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1] == 10;
     }
