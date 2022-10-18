31758823ba3f3fac2c7bb232baa12e6166da2142
Gherardo Bertini <gherardo.bertini@kiwitron.it>
2022-10-18 16:01:09 +0200

    update Game

--- src/Game.php
+++ src/Game.php
@@ -4,11 +4,15 @@ namespace App;
 
 class Game
 {
+    private $score = 0;
+
     public function roll(int $pins): void
-    {}
+    {
+        $this->score += $pins;
+    }
 
     public function score(): int
     {
-        return 0;
+        return $this->score;
     }
 }
