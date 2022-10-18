2ef8b142a129e2d7f731f4e81e05b3c76b08f18d
Gherardo Bertini <gherardo.bertini@gmail.com>
2022-10-18 16:31:53 +0200

    Feature/third test (#3)
    * add failing testOneSpare
    
    * refactor Game
    
    * refactor using frames
    
    * add manage spare
    
    * rename frameIndex
    
    * add isSpare method
    
    * add rollSpare method in test
    
    Co-authored-by: Gherardo Bertini <gherardo.bertini@kiwitron.it>

--- src/Game.php
+++ src/Game.php
@@ -4,15 +4,31 @@ namespace App;
 
 class Game
 {
-    private $score = 0;
+    private $rolls = [];
+    private $currentRoll = 0;
 
     public function roll(int $pins): void
     {
-        $this->score += $pins;
+        $this->rolls[$this->currentRoll++] = $pins;
     }
 
     public function score(): int
     {
-        return $this->score;
+        $score = 0;
+        $frameIndex = 0;
+        for ($frame = 0; $frame < 10; $frame++) {
+            if ($this->isSpare($frameIndex)) {
+                $score += 10 + $this->rolls[$frameIndex + 2];
+            } else {
+                $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+            }
+            $frameIndex += 2;
+        }
+        return $score;
+    }
+
+    protected function isSpare(int $frameIndex)
+    {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1] == 10;
     }
 }
