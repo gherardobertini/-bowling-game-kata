c9575e571706ad80390ef813f761793840bd2a8c
Gherardo Bertini <gherardo.bertini@gmail.com>
2022-10-18 09:47:35 +0200

    Feature/first test (#1)
    * add GameTest
    
    * add testGutterGame
    
    * update testGutterGame
    
    * add roll method to Game
    
    * update testGutterGame
    
    * add score method ro Game
    
    Co-authored-by: Gherardo Bertini <gherardo.bertini@kiwitron.it>

---
+++ src/Game.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+class Game
+{
+    public function roll(int $pins): void
+    {}
+
+    public function score(): int
+    {
+        return 0;
+    }
+}
---
+++ tests/GameTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace App\Test;
+
+use App\Game;
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase
+{
+    public function testGutterGame()
+    {
+        $g = new Game();
+
+        for($i = 0; $i < 20; $i++) {
+            $g->roll(0);
+        }
+
+        $this->assertEquals(0, $g->score());
+    }
+}
