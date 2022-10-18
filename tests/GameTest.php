80576b67c4cb5952e74173c01856d1604cfc6860
Gherardo Bertini <gherardo.bertini@kiwitron.it>
2022-10-18 09:49:59 +0200

    add testAllOnes

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -17,4 +17,15 @@ class GameTest extends TestCase
 
         $this->assertEquals(0, $g->score());
     }
+
+    public function testAllOnes()
+    {
+        $g = new Game();
+
+        for($i = 0; $i < 20; $i++) {
+            $g->roll(1);
+        }
+
+        $this->assertEquals(20, $g->score());
+    }
 }
